3775d46cd9e6e50734688f3390bff76d49fb8c42
Eric Parajon <ericparajon@gmail.com>
2025-03-19 19:05:29 -0400

    added a few additional comments

--- get-historical-weathe.R
+++ get-historical-weathe.R
@@ -4,14 +4,14 @@
 # This code chunk loads (and installs required packages) and sets up the underlying url call, and parameters for the API call
 
 if (!require(httr2)) install.packages("httr2",dependencies = TRUE) # Checking if httr package is installed, and installing it (and required dependencies) if not
-library(httr2) # loading httr2 for working with web APIs
+library(httr2) # Loads the httr2 package; used for working with web APIs
 
 if (!require(jsonlite)) install.packages("jsonlite",dependencies = TRUE)  # Checking if jsonlite package is installed, and installing it (and required dependencies) if not
-library(jsonlite) # loading jsonlite to convert JSON data to R objects.
+library(jsonlite) # Loads the jsonlite; used to convert JSON data to R objects
 
 base_url <- "https://archive-api.open-meteo.com/v1/archive" # Open-Meteo API url 
-params <- list( # setting up parameters for the API query
-  latitude = 40.7128, # Latitude and longitude of Johnston Country NC 
+params <- list( # Setting up parameters for the API query
+  latitude = 40.7128, # Latitude and longitude of New York City, NY (City Hall Park)
   longitude = -74.0060, 
   start_date = "2014-01-02", # Pulling data starting on January 2nd 2014 
   end_date = "2024-12-31", # Pulling data ending on December 31st 2024.
@@ -24,26 +24,26 @@ params <- list( # setting up parameters for the API query
 # This code chunk pulls data from the Open-Meteo API (application programming interface)
 
 response <- request(base_url) |>
-  req_url_query(!!!params) |>  # Adds query parameters; more specifically  the Splice operator (!!!) is used to unpack a list of values (in this case taken from the above params) into individual arguments to use in the function. Without the splice this would pass the list as one argument and error out.
+  req_url_query(!!!params) |>  # Adds query parameters; more specifically  the Splice operator (!!!) is used to unpack a list of values (in this case taken from the above params) into individual arguments to use in the function. Without it the list would be passed as a single argument leading to an error
   req_perform()  # Sends request to API to fetch results back to R
 
-# This code checks if the API request was successful and returns an error if it was not
+# This code checks if the API request was successful (a status code of 200) and returns an error if it was not, and stops the code running
 
 if (response$status != 200) {
   stop("Failed to retrieve data. Status code: ", response$status)
 }
 
-# Checking data from API call (first 5 variables)
+# Checking data from API call for debugging purposes (first 5 variables)
 head(resp_body_json(response), 5) 
 
 # Returning content as UTF-8 string
 
 json_data <- resp_body_string(response) 
 
-# checking output of small portion of json_data (first 100 characters)
+# checking output of small portion of json_data for debugging purposes (first 100 characters)
 print(substr(json_data, 1, 100))
 
-# This code flattens the nested jason data into an R object
+# This code flattens the nested JSON data returned from the API into an R object which is easier to work with
 weather_data <- fromJSON(json_data, flatten = TRUE)
 
 # This code chunk checks if the API response was valid and returned the expected hourly variables
@@ -63,10 +63,10 @@ str(hourly)
 # Creating a dataframe of relevant variables
 df <- data.frame(
   time = as.POSIXct(hourly$time, format = "%Y-%m-%dT%H:%M", tz = "America/New_York"), # formats the time variable to interpret time like 2024-01-02T00:00
-  temperature = hourly$temperature_2m,
-  precipitation = hourly$precipitation,
-  relative_humidity = hourly$relative_humidity_2m,
-  dew_point = hourly$dew_point_2m
+  temperature = hourly$temperature_2m, # temperatue measured in F
+  precipitation = hourly$precipitation, # precipitation measured in mm
+  relative_humidity = hourly$relative_humidity_2m, # relative humidity %
+  dew_point = hourly$dew_point_2m # dew point measured in F
 )
 
 # Inspecting the newly generated dataframe
@@ -77,7 +77,7 @@ str(df)
 
 png(filename = "weather_plots.png", width = 1400, height = 1400) # Line sets the download parameters for the weather_plots
 
-par(mfrow = c(2, 2), mar = c(4, 4, 3, 1)) # This line specifies the organization of the following plots first arranging them on a 2x2 panel, then setting the margins (bottom, left, top, right)
+par(mfrow = c(2, 2), mar = c(4, 4, 3, 1)) # This line specifies the organization of the following plots first arranging them on a 2x2 grid, then setting the margins (bottom, left, top, right)
 
 # Plot Panel 1: Temperature over Time
 plot(df$time, df$temperature, type = "l", col = "blue", # Creating a lineplot (type="l") of temperature over time
@@ -99,10 +99,10 @@ plot(df$time, df$dew_point, type = "l", col = "blue",
      main = "Dew Point over Time", # title
      xlab = "Time", ylab = "Dew Point (°F)") # axis labels
 
-# Add a title to all four panels (the title is outside the panels)
+# Add a title to all four panels (the title is outside the panels). Mtext is used to add text to the outer margins of the plot, the remaining arguements control placements and aesthetics
 mtext("Weather Data Overview", side = 3, line = - 2, outer = TRUE,cex = 2.2, col = "black", font = 2)  # side determines horizontal placement, line vertical, outer makes it outside the plotting area, cex controls the font size, col sets the text color to black, font sets the font to bold
 
-par(fig = c(0.5, 1, 0.5, 1), new = TRUE) # This defines the top-right area of the second panel so I can add the source and author information
+par(fig = c(0.5, 1, 0.5, 1), new = TRUE) # This defines a new plotting region in the top-right area of the second panel so I can add the source and author information, by using new=TRUE the existing four weather plots are kept and the new text can be added on top
 mtext("Source: Open-Meteo Historical Weather API | Author: Eric Parajon (using code modified from John D. Martin III)", 
       side = 3, line = 2, cex = 0.8, col = "black", adj = 1) # adj = 1 for right alignment
 
@@ -115,7 +115,7 @@ saveRDS(df, file = "historical_weather_data.rds")
 
 # This chunk of code ensures the correct output files are in place 
 
-# First, I create a vector of default values NAs to store name(s) of missing files, this is two elements long to account for the two required files
+# First, I create a vector of default values NAs to store name(s) of missing files, this is two elements long to account for the two required files. It's initialized with NA values which are then replaced with the filenames if the files are missing
 
 missing <- c(NA, NA)
 
@@ -130,10 +130,10 @@ if (file.exists("historical_weather_data.rds")==FALSE) {
 }
 
 # Check if both the above files are present and if so output the completion message; if not indicate which file is missing
-if (sum(!is.na(missing)) == 0) { # This line ensures both values in the vector are NA (by counting the number of non NA) before printing the completion message 
+if (sum(!is.na(missing)) == 0) { # This line ensures both values in the vector are NA (by counting the number of non NA values) before printing the completion message 
   cat("Data download, visualization, and saving complete.\n") # Using calls to concatenate to print output
 } else {
   # List missing file(s) if required
   cat("The required file(s) are missing:\n")
-  cat(na.omit(missing), sep = "\n")
+  cat(na.omit(missing), sep = "\n") # na.omit(missing) removes the NA values from the missing vector, so that only the missing file names are printed
 }
